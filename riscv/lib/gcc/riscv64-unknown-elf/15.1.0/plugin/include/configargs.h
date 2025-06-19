695081122a075161998b3a5ca99947f37356e3a4
Pulkit Pareek <pulkitpareek18@gmail.com>
2025-06-19 11:27:41 +0530

    Track large RISC-V toolchain files with Git LFS

---
+++ riscv/lib/gcc/riscv64-unknown-elf/15.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/pulkit/riscv-compiler/riscv-gnu-toolchain/gcc/configure --target=riscv64-unknown-elf --prefix=/home/pulkit/riscv --disable-shared --disable-threads --enable-languages=c,c++ --with-pkgversion=g1b306039a --with-system-zlib --enable-tls --with-newlib --with-sysroot=/home/pulkit/riscv/riscv64-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-tm-clone-registry --src=.././gcc --disable-multilib --with-abi=lp64 --with-arch=rv64gc --with-tune=rocket --with-isa-spec=20191213 'CFLAGS_FOR_TARGET=-Os    -mcmodel=medlow' 'CXXFLAGS_FOR_TARGET=-Os    -mcmodel=medlow'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64" }, { "arch", "rv64imafdc_zicsr_zifencei_zaamo_zalrsc" }, { "tune", "rocket" }, { "isa_spec", "20191213" } };
